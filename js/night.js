e158e8d84422f1f5970dbdba32cedb9e59b40cab
daniel-9527 <845970490@qq.com>
2020-04-03 15:31:08 +0800

    Site updated: 2020-04-03 15:31:06

--- js/night.js
+++ js/night.js
@@ -1,1 +1,41 @@
-"use strict";var nightNav,isNight=localStorage.getItem("night");function applyNight(t){"true"===t.toString()?document.body.classList.add("night"):document.body.classList.remove("night")}function findNightNav(){(nightNav=document.getElementById("night-nav"))?nightNav.addEventListener("click",switchNight):setTimeout(findNightNav,100)}function switchNight(){applyNight(isNight=!isNight||"true"!==isNight.toString()),localStorage.setItem("night",isNight)}findNightNav(),applyNight(isNight);
+/**
+ * Icarus 夜间模式 by iMaeGoo
+ * https://www.imaegoo.com/
+ */ 
+var isNight = localStorage.getItem('night');
+var nightNav;
+
+function applyNight(value) {
+    if (value.toString() === 'true') {
+        document.body.classList.add('night');
+    } else {
+        document.body.classList.remove('night');
+    }
+}
+
+function findNightNav() {
+    nightNav = document.getElementById('night-nav');
+    if (!nightNav) {
+        setTimeout(findNightNav, 100);
+    } else {
+        nightNav.addEventListener('click', switchNight);
+    }
+}
+
+function switchNight() {
+    isNight = isNight ? isNight.toString() !== 'true' : true;
+    applyNight(isNight);
+    localStorage.setItem('night', isNight);
+}
+
+let mode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+if(mode.toString() != localStorage.getItem('current')){
+    localStorage.setItem('switched', 'false');
+    localStorage.setItem('current', window.matchMedia('(prefers-color-scheme: dark)').matches);
+}
+if(localStorage.getItem('switched') != 'true'){
+    isNight = window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
+findNightNav();
+applyNight(isNight);
